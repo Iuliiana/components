38e5520c85cde182b754e9900d8f7117c0c7b5fd
Iuliia <tretiakova.iuliia@gmail.com>
2022-07-03 19:50:56 +0300

    components

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import ShopItemModel from '../models/ShopItemModel';
+
+class ShopItemClass extends React.Component {
+
+    render() {
+        const { item } = this.props;
+
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+ShopItemClass.propTypes = {
+    item: propTypes.instanceOf(ShopItemModel).isRequired
+};
+
+export default ShopItemClass;
---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,30 @@
+import propTypes from 'prop-types';
+import ShopItemModel from '../models/ShopItemModel';
+
+function ShopItemFunc(props) {
+    const { item } = props;
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+ShopItemFunc.propTypes = {
+    item: propTypes.instanceOf(ShopItemModel).isRequired
+};
+
+export default ShopItemFunc;
